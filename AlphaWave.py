d1dfadbbbb25c08b0bbfb76591cfd340cefee887
jurchiks33 <jurisslisans@gmail.com>
2024-03-15 18:18:37 +0100

    work on a frames in main page

--- AlphaWave.py
+++ AlphaWave.py
@@ -21,6 +21,12 @@ def main():
     #Set initial size and position of the window.
     root.geometry(f'{width}x{height}+{x_offset}+{y_offset}')
 
+    #Here coming frames for the pages.
+    frame1 = tk.Frame(root, bg='silver', bd=2, relief='solid')
+
+
+    #Place frames in layout.
+    frame1.place(relx=0.5, rely=0.05, relwidth=0.2, relheight=0.2)
 
     #Tkinter event loop.
     root.mainloop()
