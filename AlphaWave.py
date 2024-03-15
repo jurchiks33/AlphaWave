8d0495594724c4ffd73537094b4935bc9d969cdd
jurchiks33 <jurisslisans@gmail.com>
2024-03-15 19:19:38 +0100

    for on a frames for a main window

--- AlphaWave.py
+++ AlphaWave.py
@@ -25,12 +25,14 @@ def main():
     frame1 = tk.Frame(root, bg='#dae1f3', bd=1, relief='solid')
     frame2 = tk.Frame(root, bg='#d9d9d9', bd=1, relief='solid')
     frame3 = tk.Frame(root, bg='#b0bac3', bd=0.5, relief='solid')
+    #frame4 = 
 
 
     #Place frames in layout.
     frame1.place(relx=0, rely=0, relwidth=0.25, relheight=0.6)
     frame2.place(relx=0.3, rely=0, relwidth=0.64, relheight=0.4)
     frame3.place(relx=0.3, rely=0.40, relwidth=0.16, relheight=0.03)
+    #frame4.place
 
     #Tkinter event loop.
     root.mainloop()
