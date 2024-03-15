724ea5f1bc2fe722a34443098a55a91f576be933
jurchiks33 <jurisslisans@gmail.com>
2024-03-15 17:56:00 +0100

    work on a main display

--- AlphaWave.py
+++ AlphaWave.py
@@ -5,4 +5,13 @@ def main():
     root = tk.TK()
     root.title("AlphaWave- Trading Platform")
 
-    
+    #Background color.
+    root.configure(bg='1a1a1a')
+
+    #Calculate dimensions  for the window to be 80% of screen size.
+    screen_width = root.winfo_screenwidth()
+    screen_height = root.winfo_screenheight()
+    width = int(screen_width * 0.8)
+    height = int(screen_height * 0.8)
+
+    #Center the window screen.
