a655df204c27cfc4ca98825d72f4d02fb0c333db
jurchiks33 <jurisslisans@gmail.com>
2024-03-15 18:32:46 +0100

    work on a frames in a main page

--- AlphaWave.py
+++ AlphaWave.py
@@ -22,11 +22,13 @@ def main():
     root.geometry(f'{width}x{height}+{x_offset}+{y_offset}')
 
     #Here coming frames for the pages.
-    frame1 = tk.Frame(root, bg='silver', bd=2, relief='solid')
+    frame1 = tk.Frame(root, bg='#dae1f3', bd=2, relief='solid')
+    frame2 = tk.Frame(root, bg='white', bd=2, relief='solid')
 
 
     #Place frames in layout.
-    frame1.place(relx=0.5, rely=0.05, relwidth=0.2, relheight=0.2)
+    frame1.place(relx=0, rely=0, relwidth=0.25, relheight=0.6)
+    frame2.place(relx=0.3, rely=0, relwidth=0.64, relheight=0.4)
 
     #Tkinter event loop.
     root.mainloop()
